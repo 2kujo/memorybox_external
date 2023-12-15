d131759dd26636f19fc7fe401caa514b1134a47d
kmj <rlaalswh531@gmail.com>
2023-12-15 14:35:23 +0900

    Initial commit

---
+++ src/main/java/com/memorybox/MemoryboxExternalApplication.java
@@ -0,0 +1,13 @@
+package com.memorybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryboxExternalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoryboxExternalApplication.class, args);
+    }
+
+}
