c4b69a28ae55a5ed594970fe4a1cfbd1c19658b5
김현교 <zkdlajt96@gmail.com>
2023-12-21 12:36:05 +0900

    :bug: fix: API 필드 명 수정

--- src/main/java/com/memorybox/dto/AccountCreateResponseDto.java
+++ src/main/java/com/memorybox/dto/AccountCreateResponseDto.java
@@ -4,7 +4,7 @@ import com.memorybox.domain.corebank.entity.CoreBank;
 
 import java.time.LocalDate;
 
-public record AccountCreateResponseDto(long coreBankId, String accountName, int balance, String productName, LocalDate startDate, LocalDate maturityDate) {
+public record AccountCreateResponseDto(long coreBankId, String accountNum, int balance, String productName, LocalDate startDate, LocalDate maturityDate) {
     public AccountCreateResponseDto(CoreBank entity){
         this(entity.getId(), entity.getAccountNum(), entity.getBalance(), entity.getProductName(), entity.getStartDate(), entity.getMaturityDate());
 
